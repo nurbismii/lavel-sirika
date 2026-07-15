0786aa4f29daf90f7d4ce50e8040b3de5bc0d5c3
Nurbismii <nurbismi74@gmail.com>
2026-07-15 11:02:46 +0800

    fix: time location & ignore outputs

--- config/app.php
+++ config/app.php
@@ -67,8 +67,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    //'timezone' => 'UTC',
+    'timezone' => 'Asia/Kuala_Lumpur',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -80,8 +80,8 @@ return [
     |
     */
 
-    'locale' => 'en',
-
+    // 'locale' => 'en',
+    'locale' => 'id',
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -93,8 +93,8 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
-
+    // 'fallback_locale' => 'en',
+    'fallback_locale' => 'id',
     /*
     |--------------------------------------------------------------------------
     | Faker Locale
@@ -106,8 +106,8 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
-
+    // 'faker_locale' => 'en_US',
+    'faker_locale' => 'id_ID',
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
